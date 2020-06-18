647627acad7213a3a26b2c1ba08083964b18c2d1
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-06-18 08:15:09 +0700

    Add files via upload

---
+++ drinks.php
@@ -0,0 +1,97 @@
+<?php
+  include 'inc/header.php';
+ ?>
+
+ <div class="main">
+    <div class="content">
+    	<div class="content_top">
+    		<div class="clear"></div>
+    	</div>
+	     <div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details.php"><img src="images/hinh_6.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/hinh_7.1.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/hinh_8.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/hinh_9.jpg" alt="" />
+					 <h2>Coffee </h2>					 
+					 <p><span class="price">50.000VND</span></p>
+
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details.php"><img src="images/hinh_6.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/hinh_7.1.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/hinh_8.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/hinh_9.jpg" alt="" />
+					 <h2>Coffee </h2>					 
+					 <p><span class="price">50.000VND</span></p>
+
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+			<div class="section group">
+				<div class="grid_1_of_4 images_1_of_4">
+					 <a href="details.php"><img src="images/hinh_6.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/hinh_7.1.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p> 
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php"><img src="images/hinh_8.jpg" alt="" /></a>
+					 <h2>Coffee </h2>
+					 <p><span class="price">50.000VND</span></p>
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+				<div class="grid_1_of_4 images_1_of_4">
+				 <img src="images/hinh_9.jpg" alt="" />
+					 <h2>Coffee </h2>					 
+					 <p><span class="price">50.000VND</span></p>
+
+				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				</div>
+			</div>
+    </div>
+ </div>
+
+<?php
+  include 'inc/footer.php';
+ ?>
+
