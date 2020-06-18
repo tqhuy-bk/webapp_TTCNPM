e51f6abef8e36cd57f3e9ad7b057c590a3f966b2
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-06-18 08:16:08 +0700

    Add files via upload

---
+++ inc/slider.php
@@ -0,0 +1,16 @@
+
+	<div class="slide_container">
+		   <!-- FlexSlider -->
+			<section class="slider">
+				  <div class="flexslider">
+					<ul class="slides">
+						<li class="art_slide"><img src="images/hinh_10.png" alt=""/></li>
+						<li class="art_slide"><img src="images/hinh_11.png" alt=""/></li>
+						<li class="art_slide"><img src="images/hinh_12.png" alt=""/></li>
+						<li class="art_slide"><img src="images/hinh_13.png" alt=""/></li>
+				    </ul>
+				  </div>
+	      </section>
+          <!-- FlexSlider -->
+	  <div class="clear"></div>	
+    </div>
