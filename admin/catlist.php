f51bb65315a0255b7de785d0e6eb71bb459ade45
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-06-18 08:21:31 +0700

    Add files via upload

---
+++ admin/catlist.php
@@ -0,0 +1,54 @@
+﻿<?php include 'inc/header.php';?>
+<?php include 'inc/sidebar.php';?>
+<?php 
+    include '../classes/category.php';
+ ?>
+<?php 
+    $show = new category();
+ ?>
+        <div class="grid_10">
+            <div class="box round first grid">
+                <h2>Category List</h2>
+                <div class="block">        
+                    <table class="data display datatable" id="example">
+					<thead>
+						<tr>
+							<th>Serial No.</th>
+							<th>Category Name</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+						   $showcategory= $show->show_category();
+						   if($showcategory){
+						   	  $i=0;
+						   	  while($result = $showcategory->fetch_assoc()){ 
+						   	  	$i++;
+						   //result trả về một cấu trúc gồm id và name
+						 ?>
+						<tr class="even gradeC">
+							<td><?php  echo $i ?></td>
+							<td><?php echo $result['catName'] ?> </td> 
+							<!-- echo là in ra catName -->
+							<td><a href="category_edit.php?catid=<?php echo $result['catID'] ?>">Edit</a> || <a href="?catid=<?php echo $result['catID'] ?>">Delete</a></td>
+						</tr>
+					  <?php 
+					     }
+					    }
+					   ?>
+					</tbody>
+				</table>
+               </div>
+            </div>
+        </div>
+<script type="text/javascript">
+	$(document).ready(function () {
+	    setupLeftMenu();
+
+	    $('.datatable').dataTable();
+	    setSidebarHeight();
+	});
+</script>
+<?php include 'inc/footer.php';?>
+
