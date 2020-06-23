0d99b4e3fe4f2316edb68ba94c91636d2b147145
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-06-23 10:27:44 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -8,13 +8,22 @@ include 'inc/header.php' ?>
  ?>
 <div class="main">
     <div class="content">
-    	<p style="color:red;font-size:30px;margin-left:20%;"> Dạ nhóm em chưa làm xong trang này</p>
-    	<br>
-    	<br>
-    	<br>
-    	<br>
-    	<br>
-    	<br>
+    	<!-- <p style="color:red;font-size:30px;margin-left:20%;"> Dạ nhóm em chưa làm xong trang này</p> -->
+    	<div style="background:#dbdee0;width:100%;margin:auto;height:500px;">
+            <table class="tblone">
+                <tr><th></th></tr>
+            </table>
+            <div style="font-size:25px;margin:2% 30%;"><h1 style="float:left;margin-right:10%;">Name:</h1><p> Trần Quang Huy1 </p></div>
+            <div style="font-size:25px;margin:2% 30%;"><h1 style="float:left;margin-right:10%;">Email:</h1><p> huy@gmail.com </p></div>
+            <div style="font-size:25px;margin:2% 30%;"><h1 style="float:left;margin-right:10%;">Phone:</h1><p> 65222625625625</p></div>
+            <div style="font-size:25px;margin:2% 30%;"><h1 style="float:left;margin-right:10%;">Balance:</h1><p> 651561VND </p></div>
+            <br>
+            <br>
+            <div style="margin:2% 30%;"> 
+                <a href="changeProfiles.php"style="background: #0c5a6c;float:left; margin-right:40px;" class="buysubmit"> Edit Information </a>
+                <a href="drawmoney.php"style="background: #0c5a6c;float:left;" class="buysubmit"> Draw Money </a>
+            </div>
+        </div>
     </div> 
 </div>
 <?php 
