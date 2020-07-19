fcca85bbb8a11935c8a89175404343f23626418f
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-19 17:36:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 	      	        if($get_product_combo){
 	      	        	while($result=$get_product_combo->fetch_assoc()){//lay du lieu vao bien result
 	      	     ?>
-				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;">
-					 <a href="details.php"><img src="admin/uploads/<?php echo $result['image'] ?>" alt="" /></a>
+				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;margin: 1% 0% 1% 0%;">
+					 <a href="details.php?productid=<?php echo $result['productID'] ?>"><img src="admin/uploads/<?php echo $result['image'] ?>" alt="" /></a>
 					 <h2><?php echo $result['productName'] ?></h2>
 					 <p><?php echo $fm->textShorten($result['description'],30) ?></p>
 					 <p><span class="price"><?php echo $result['price'] ?>VND</span></p>
@@ -29,37 +29,28 @@
 				 ?>
 			</div>
 			<div class="content_bottom">
-    		<div class="heading">
-    		<h3>Drinks</h3>
-    		</div>
-    		<div class="clear"></div>
-    	</div>
+    		     <div class="heading">
+    		        <h3>Discount</h3>
+    		     </div>
+    		     <div class="clear"></div>
+    	    </div>
 			<div class="section group">
-				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;">
-					 <a href="details.php"><img src="images/hinh_6.jpg" alt="" /></a>
-					 <h2>Coffee </h2>
-					 <p><span class="price">50.000VND</span></p>
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				 <?php 
+	      	        $get_product_discount= $product->get_discount();
+	      	        if($get_product_discount){
+	      	        	while($result=$get_product_discount->fetch_assoc()){//lay du lieu vao bien result
+	      	     ?>
+				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;margin: 1% 0% 1% 0%;">
+					 <a href="details.php?productid=<?php echo $result['productID'] ?>"><img src="admin/uploads/<?php echo $result['image'] ?>" alt="" /></a>
+					 <h2><?php echo $result['productName'] ?></h2>
+					 <p><?php echo $fm->textShorten($result['description'],30) ?></p>
+					 <p><span class="price"><?php echo $result['price'] ?>VND</span></p>
+				     <div class="button"><span><a href="details.php?productid=<?php echo $result['productID'] ?>" class="details">Details</a></span></div>
 				</div>
-				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;">
-					<a href="details.php"><img src="images/hinh_7.1.jpg" alt="" /></a>
-					 <h2>Coffee </h2>
-					 <p><span class="price">50.000VND</span></p> 
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;">
-					<a href="details.php"><img src="images/hinh_8.jpg" alt="" /></a>
-					 <h2>Coffee </h2>
-					 <p><span class="price">50.000VND</span></p>
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4" style=" min-height:390px;">
-				 <img src="images/hinh_9.jpg" alt="" />
-					 <h2>Coffee </h2>					 
-					 <p><span class="price">50.000VND</span></p>
-
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
-				</div>
+				<?php 
+					  }
+	      	        }
+				 ?>
 			</div>
     </div>
  </div>
