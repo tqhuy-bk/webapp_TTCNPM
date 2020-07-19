b04207252755ce494b0922e793e513572184c482
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-19 17:52:22 +0700

    cập nhập và lấy thông tin admin

--- classes/adminlogin.php
+++ classes/adminlogin.php
@@ -52,5 +52,33 @@
             	}
             }
     	}
+       public function get_info_admin(){
+            $id=Session::get('adminID');
+            $query = "SELECT * FROM tbl_admin WHERE adminID = '$id' ";
+            $result = $this->db->select($query);
+            return $result;
+        }
+        public function update_info_customer($data,$id){
+            $adminName = mysqli_real_escape_string($this->db->link, $data['adminName']);
+            $adminEmail = mysqli_real_escape_string($this->db->link, $data['adminEmail']);
+            $adminUser = mysqli_real_escape_string($this->db->link, $data['adminUser']); 
+            $role = mysqli_real_escape_string($this->db->link, $data['role']);          
+            if(empty($adminName) || empty($adminEmail) || empty($adminPhone)){
+                $alert= "<span style='color:red;font-size:16px;'> Must be not empty</span>";
+                return $alert;
+            }
+            else{                 
+                    $query ="UPDATE tbl_admin  SET adminName ='$adminName',adminEmail='$adminEmail',adminUser='$adminUser' WHERE adminID = '$id' ";
+                    $result = $this->db->update($query);
+                    if($result){
+                         $alert="<span style='color:green;font-size:16px;margin:2% 20%;'> Update completion</span>";
+                         return $alert;
+                    }
+                    else{
+                         $alert="<span style='color:red;font-size:16px;margin:2% 35%;'> Update not completion</span>";
+                         return $alert;
+                    }
+            }
+        }
     }
  ?>
