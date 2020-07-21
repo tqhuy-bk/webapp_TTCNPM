1845eccdd62f997df881bea3b9f6e2c2185e69bc
leequangminh2272000 <67750897+leequangminh2272000@users.noreply.github.com>
2020-07-21 23:05:19 +0700

    Update sidebar.php
    distribute authorities

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -1,6 +1,5 @@
-<?php
-  $role=Session::get('role');
-?>
+
+
 <div class="grid_2">
     <div class="box sidemenu">
         <div class="block" id="section-menu">
@@ -13,13 +12,14 @@
                         
                     </ul>
                 </li> -->
+
+                <?php $level= Session::get('level')?>
                 <li><a href="infoAdmin.php"class="menuitem" style=" background:#ef9478;">My account</a></li>
                 <?php 
-                    if($role=='owner_stall'){
+                    if($level==1){
                       echo '
                       <li><a class="menuitem" style=" background:#ef9478;">Product</a>
                             <ul class="submenu">
-                                <li><a href="productadd.php">Add Product</a> </li>
                                 <li><a href="productlist.php">Product List</a> </li>
                             </ul>
                       </li>
@@ -31,14 +31,96 @@
                       </li>
                      <li><a class="menuitem" style=" background:#ef9478;">Category Option</a>
                             <ul class="submenu">
-                                <li><a href="catadd.php">Add Category</a> </li>
+                                <li><a href="catadd.php">Add New Category</a> </li>
+                                <li><a href="catlist.php">Category List</a> </li>
+                            </ul>
+                     </li>
+                     <li><a class="menuitem" style=" background:#ef9478;">Members</a>
+                                <ul class="submenu">
+                                <li><a href="memeber.php">Member List</a> </li>
+                            </ul>
+                        </li>
+                      ';
+                    }
+                 ?>
+                 <?php 
+                    if($level==2){
+                      echo '
+                      <li><a class="menuitem" style=" background:#ef9478;">Product</a>
+                            <ul class="submenu">
+                                <li><a href="productlist.php">Product List</a> </li>
+                            </ul>
+                      </li>
+                      <li><a class="menuitem" style=" background:#ef9478;">Vendor Option</a>
+                            <ul class="submenu">
+                                <li><a href="vendorlist.php">Vendor List</a> </li>
+                            </ul>
+                      </li>
+                     <li><a class="menuitem" style=" background:#ef9478;">Category Option</a>
+                            <ul class="submenu">
                                 <li><a href="catlist.php">Category List</a> </li>
                             </ul>
                      </li>
                       ';
                     }
                  ?>
-				 
+				 <?php 
+                    if($level==3){
+                      echo '
+                      <li><a class="menuitem" style=" background:#ef9478;">Product</a>
+                            <ul class="submenu">
+                                <li><a href="productadd.php">Add Product</a> </li>
+                                <li><a href="productlist.php">Product List</a> </li>
+                            </ul>
+                      </li>
+                     <li><a class="menuitem" style=" background:#ef9478;">Category Option</a>
+                            <ul class="submenu">
+                                <li><a href="catadd.php">Add Category</a> </li>
+                                <li><a href="catlist.php">Category List</a> </li>
+                            </ul>
+                     </li>
+                      <li><a class="menuitem" style=" background:#ef9478;">Order</a>
+                    <ul class="submenu">
+                        <li><a href="vieworder.php"> View Order</a> </li>
+                    </ul>
+                </li>
+            
+                <li><a class="menuitem" style=" background:#ef9478;" href="warelist.php">Nhà kho</a>
+                    <ul class="submenu">
+                        <li><a href="wareordersend.php">Yêu cầu nhập kho</a> </li>
+                    </ul>
+                </li>
+                      ';
+                    }
+                 ?>
+                 <?php 
+                    if($level==4){
+                      echo '
+                      <li><a class="menuitem" style=" background:#ef9478;">Product</a>
+                            <ul class="submenu">
+                                <li><a href="productlist.php">Product List</a> </li>
+                            </ul>
+                      </li>
+                     <li><a class="menuitem" style=" background:#ef9478;">Category Option</a>
+                            <ul class="submenu">
+                                <li><a href="catadd.php">Add Category</a> </li>
+                                <li><a href="catlist.php">Category List</a> </li>
+                            </ul>
+                     </li>
+                      <li><a class="menuitem" style=" background:#ef9478;">Order</a>
+                    <ul class="submenu">
+                        <li><a href="vieworder.php"> View Order</a> </li>
+                    </ul>
+                </li>
+            
+                <li><a class="menuitem" style=" background:#ef9478;" href="warelist.php">Nhà kho</a>
+                    <ul class="submenu">
+                        <li><a href="wareordersend.php">Yêu cầu nhập kho</a> </li>
+                    </ul>
+                </li>
+                      ';
+                    }
+                 ?>
                 <!-- <li><a class="menuitem">Update Pages</a>
                     <ul class="submenu">
                         <li><a>About Us</a></li>
@@ -52,17 +134,7 @@
                     </ul>
                 </li> -->
                  
-                <li><a class="menuitem" style=" background:#ef9478;">Order</a>
-                    <ul class="submenu">
-                        <li><a href="vieworder.php"> View Order</a> </li>
-                    </ul>
-                </li>
-		    
-                <li><a class="menuitem" style=" background:#ef9478;" href="warelist.php">Nhà kho</a>
-                    <ul class="submenu">
-                        <li><a href="wareordersend.php">Yêu cầu nhập kho</a> </li>
-                    </ul>
-                </li>
+               
                 <br>
                 <br>
                 <br>
