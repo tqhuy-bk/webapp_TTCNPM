4b3a8af4c3047fd84fdc9ecc7bebe9e22db835ca
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-27 08:33:54 +0700

    thêm function format tiền

--- helpers/format.php
+++ helpers/format.php
@@ -33,5 +33,18 @@ class Format{
     }
     return $title = ucfirst($title);
    }
+ public function format_money($n=0){// dùng để thêm dấu chấm vào trong hiển thị số tiền
+    $n=(string)$n;
+    $n=strrev($n);
+    $res='';
+    for($i=0;$i<strlen($n);$i++){
+        if($i%3==0 && $i!= 0){
+            $res.='.';
+        }
+        $res.=$n[$i];
+    }
+    $res = strrev($res);
+    return $res;
+  }
 }
 ?>
