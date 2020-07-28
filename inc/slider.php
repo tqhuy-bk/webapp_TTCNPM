76c0c4f1392fe3ea06e58d928e563fe3bdd51c90
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-28 08:19:32 +0700

    Update slider.php

--- inc/slider.php
+++ inc/slider.php
@@ -1,13 +1,13 @@
 
-	<div class="slide_container" style="margin-top:10px;">
+	<div class="slide_container" style="margin-top:10px;min-height: 300px;">
 		   <!-- FlexSlider -->
 			<section class="slider">
-				  <div class="flexslider">
+				  <div class="flexslider" style="min-height: :200px;">
 					<ul class="slides">
-						<li class="art_slide"><img src="images/hinh_10.png" alt=""/></li>
-						<li class="art_slide"><img src="images/hinh_11.png" alt=""/></li>
-						<li class="art_slide"><img src="images/hinh_12.png" alt=""/></li>
-						<li class="art_slide"><img src="images/hinh_13.png" alt=""/></li>
+						<li class="art_slide" style="min-height: :200px;"><img src="images/hinh_10.png" alt=""/></li>
+						<li class="art_slide" style="min-height: :200px;"><img src="images/hinh_11.png" alt=""/></li>
+						<li class="art_slide" style="min-height: :200px;"><img src="images/hinh_12.png" alt=""/></li>
+						<li class="art_slide" style="min-height: :200px;"><img src="images/hinh_13.png" alt=""/></li>
 				    </ul>
 				  </div>
 	      </section>
