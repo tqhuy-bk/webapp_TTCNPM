8696c109fc2b62150ab0eb352a6e134bc2b3e926
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-28 08:21:46 +0700

    Update product.php

--- classes/product.php
+++ classes/product.php
@@ -1,27 +1,22 @@
-
 <?php 
    $filepath= realpath(dirname(__FILE__));
    include_once ($filepath.'/../lib/database.php');
    include_once ($filepath.'/../helpers/format.php');
 
  ?>
-
  <?php 
-    /**
-     * 
-     */
     class product
     {
-    	private $db;
-    	private $fm;
+        private $db;
+        private $fm;
 
-    	public function __construct()
-    	{
-    		$this->db = new Database();
-    		$this->fm = new Format();
-    	}
-    	public function insert_product($data,$files)
-    	{
+        public function __construct()
+        {
+            $this->db = new Database();
+            $this->fm = new Format();
+        }
+        public function insert_product($data,$files)
+        {
            
             $productName = mysqli_real_escape_string($this->db->link, $data['productName']);
             $category = mysqli_real_escape_string($this->db->link, $data['category']);
@@ -30,27 +25,21 @@
             $price = mysqli_real_escape_string($this->db->link, $data['price']);
             $type = mysqli_real_escape_string($this->db->link, $data['type']);
 
-
-            //kiem tra va cho vao foad
-            // $permited = array('jpg','jpeg','png','gif');
             $file_name = $_FILES['image']['name'];
-            // $file_size = $_FILES['image']['size'];
-            // $file_temp = $_FILES['image']['tmp_name'];
             
             $div =explode('.', $file_name);
             $file_ext = strtolower(end($div));
             $unique_image = substr(md5(time()), 0,10).'.'.$file_ext;
-            // $uploaded_image = "D/xampp/htdocs/shop/admin/uploads".$unique_image;
             
 
             if($productName=="" || $category=="" || $vendor=="" || $description=="" || $price=="" || $file_name=="" || $type==""){
-            	$alert= "<span class='error' > fiels must be not empty</span>";
-            	return $alert;
+                $alert= "<span class='error' > Must be not empty</span>";
+                return $alert;
             }
             else{
                 move_uploaded_file($_FILES['image']['tmp_name'], "uploads/$file_name");
-            	$query ="INSERT INTO tbl_product(productName,categoryID,vendorID,description,price,type,image) VALUES('$productName','$category','$vendor','$description','$price','$type','$file_name')"; 
-            	$result = $this->db->insert($query);
+                $query ="INSERT INTO tbl_product(productName,categoryID,vendorID,description,price,type,image) VALUES('$productName','$category','$vendor','$description','$price','$type','$file_name')"; 
+                $result = $this->db->insert($query);
                 if($result){
                     $alert="<span class ='success'> Insert product completion</span>";
                     return $alert;
@@ -60,11 +49,11 @@
                     return $alert;
                 }
             }
-    	}
+        }
         public function show_product(){
             $query = "
-            SELECT p.*,c.catName
-            FROM tbl_product as p,tbl_categori as c WHERE p.categoryID = c.catID
+            SELECT p.*,c.catName,v.vendorName
+            FROM tbl_product as p,tbl_categori as c,tbl_vendor as v WHERE p.categoryID = c.catID AND p.vendorID= v.vendorID
              order by p.productID desc";
             //lấy các phần tử trong bảng rồi sắp xếp theo ID
             $result = $this->db->select($query);
@@ -85,20 +74,16 @@
             $type = mysqli_real_escape_string($this->db->link, $data['type']);
             
 
-            //kiem tra va cho vao foad
-            // $permited = array('jpg','jpeg','png','gif');
             $file_name = $_FILES['image']['name'];
-            // $file_size = $_FILES['image']['size'];
-            // $file_temp = $_FILES['image']['tmp_name'];
+            
             
             $div =explode('.', $file_name);
             $file_ext = strtolower(end($div));
             $unique_image = substr(md5(time()), 0,10).'.'.$file_ext;
-            // $uploaded_image = "D/xampp/htdocs/shop/admin/uploads".$unique_image;
             
 
             if(empty($productName) || empty($category) || empty($vendor) || empty($description) || empty($price)){
-                $alert= "<span class='error'> Fiels must be not empty</span>";
+                $alert= "<span class='error'> Must be not empty</span>";
                 return $alert;
             }
             else{ 
@@ -137,31 +122,42 @@
         ///////////////
         public function get_combo(){
             $query = "SELECT * FROM tbl_product WHERE type='combo'";
-            //chọn phần tử trong bảng với đk catID= id 
+            $result = $this->db->select($query);
+            return $result;
+        }
+        public function get_discount(){
+            $query = "SELECT * FROM tbl_product WHERE type='discount' ";
+            $result = $this->db->select($query);
+            return $result;
+        }
+        public function get_drinks(){
+            $query = "SELECT * FROM tbl_product WHERE type='drinks'";
+            //chọn phần tử trong bảng với đk type= combo 
+            $result = $this->db->select($query);
+            return $result;
+        }
+        public function get_foods(){
+            $query = "SELECT * FROM tbl_product WHERE type='foods'";
+            //chọn phần tử trong bảng với đk type= combo 
             $result = $this->db->select($query);
             return $result;
         }
         public function get_details($id){
             $query = "
-            SELECT p.*,c.catName
-            FROM tbl_product as p,tbl_categori as c WHERE p.categoryID = c.catID AND
-             p.productID='$id'";
+            SELECT p.*,c.catName,v.vendorName
+            FROM tbl_product as p,tbl_categori as c,tbl_vendor as v WHERE p.categoryID = c.catID AND
+             p.productID='$id' AND p.vendorID=v.vendorID";
             //lấy các phần tử trong bảng rồi sắp xếp theo ID
             $result = $this->db->select($query);
             return $result;
         }
-       public function search($search){
+         public function search($search){
             // Dùng câu lênh like trong sql và sứ dụng toán tử % của php để tìm kiếm dữ liệu chính xác hơn.
-            $query = "SELECT * FROM tbl_product WHERE productName LIKE '%$search%'";
+            $query = "SELECT * FROM tbl_product WHERE productName or description LIKE '%$search%'";
             // Thực thi câu truy vấn
             $result = $this->db->select($query);
             // Nếu có kết quả thì hiển thị, ngược lại thì thông báo không tìm thấy kết quả
-            if ($search != "") {
-                return $result;
-            } 
-            else {
-                echo "Khong tim thay ket qua!";
-            }
+           return $result;
         }
     }
  ?>
