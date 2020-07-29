b740001acbaf344279c7c5bbba0620d11bbf29d9
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-29 09:21:05 +0700

    Update wareadd.php

--- admin/wareadd.php
+++ admin/wareadd.php
@@ -12,7 +12,7 @@
  ?>
 <div class="grid_10">
     <div class="box round first grid">
-        <h2>Thêm hàng mới vào kho</h2>
+        <h2>Thêm hàng vào kho</h2>
         <?php  
         if(isset($check_insert_product)){
             echo $check_insert_product;
@@ -22,27 +22,27 @@
          <form action="wareadd.php" method="post" enctype="multipart/form-data">
         <!-- enctype để có thể thêm hình ảnh  -->
             <table class="form">
-               
+
                 <tr>
                     <td>
-                        <label>Tên</label>
+                        <label>Name</label>
                     </td>
                     <td>
-                        <input type="text" name="productName" placeholder="Nhập tên hàng..." class="medium" />
+                        <input type="text" name="productName" placeholder="Enter Product Name..." class="medium" />
                     </td>
                 </tr>
 				        <tr>
                     <td>
-                        <label>Số lượng</label>
+                        <label>Amount</label>
                     </td>
                     <td>
-                        <input type="text" name="amount" placeholder="Nhập số lượng..." class="medium" />
+                        <input type="text" name="amount" placeholder="Enter Amount..." class="medium" />
                     </td>
                 </tr>
 				        <tr>
                     <td></td>
                     <td>
-                        <input type="submit" name="submit" Value="Thêm" />
+                        <input type="submit" name="submit" Value="Save" />
                     </td>
                 </tr>
             </table>
@@ -62,5 +62,3 @@
 </script>
 <!-- Load TinyMCE -->
 <?php include 'inc/footer.php';?>
-
-
