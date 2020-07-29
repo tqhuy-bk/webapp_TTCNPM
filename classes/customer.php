6ef062212ff4455c574aa7466a6d4d736c63132a
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-07-29 09:24:40 +0700

    Update customer.php

--- classes/customer.php
+++ classes/customer.php
@@ -1,46 +1,36 @@
-
 <?php 
    $filepath= realpath(dirname(__FILE__));
    include_once ($filepath.'/../lib/database.php');
    include_once ($filepath.'/../helpers/format.php');
-
  ?>
-
  <?php 
-    /**
-     * 
-     */
     class customer
     {
     	private $db;
     	private $fm;
-
     	public function __construct()
     	{
     		$this->db = new Database();
     		$this->fm = new Format();
     	}
         public function create_customer($data){
-
             $customerName = mysqli_real_escape_string($this->db->link, $data['customerName']);
             $customerEmail = mysqli_real_escape_string($this->db->link, $data['customerEmail']);
             $customerPhone = mysqli_real_escape_string($this->db->link, $data['customerPhone']);
             $password = mysqli_real_escape_string($this->db->link, md5($data['password']));
-
-
             if(empty($customerName) || empty($customerEmail) || empty($customerPhone) || empty($password)){
-                $alert= "<span class='error' style='color:red;font-size:23px;' >Xin điền đầy đủ thông tin</span>";
+                $alert= "<span class='error' style='color:red;font-size:23px;' > Must be not empty</span>";
                 return $alert;
             }
             else{
                 $query ="INSERT INTO tbl_customer(customerName,customerEmail,customerPhone,password) VALUES('$customerName','$customerEmail','$customerPhone','$password')"; 
                 $result = $this->db->insert($query);
                 if($result){
-                    $alert="<span class ='success' style='color:green;font-size:23px;'>Tạo tài khoản mới thành công</span>";
+                    $alert="<span class ='success' style='color:green;font-size:23px;'>Create account completion</span>";
                     return $alert;
                 }
                 else{
-                    $alert="<span style='color:red;font-size:23px;' class ='error'>Không tạo được tài khoản mới!</span>";
+                    $alert="<span style='color:red;font-size:23px;' class ='error'> Create account not completion</span>";
                     return $alert;
                 }
             }
@@ -48,9 +38,8 @@
         public function signin_customer($data){
             $customerName = mysqli_real_escape_string($this->db->link, $data['customerName']);
             $password = mysqli_real_escape_string($this->db->link, md5($data['password']));
-
             if(empty($customerName) || empty($password)){
-                $alert= "<span  style='color:red;font-size:23px;' >Xin điền đầy đủ thông tin</span>";
+                $alert= "<span  style='color:red;font-size:23px;' > Must be not empty</span>";
                 return $alert;
             }
             else{
@@ -63,7 +52,7 @@
                       Session::set('customer_name',$value['customerName']);
                       header('Location:index.php');
                  }else{
-                     $alert= "<span style='color:red;font-size:23px;' >Thông tin đăng nhập không khớp với nhau!</span>";
+                     $alert= "<span style='color:red;font-size:23px;' > Name and password not match</span>";
                      return $alert;
                  }
             }
@@ -74,24 +63,59 @@
             $result = $this->db->select($query);
             return $result;
         }
+        public function get_info_customer_admin($id){
+            $query = "SELECT * FROM tbl_customer WHERE customerID = '$id' ";
+            $result = $this->db->select($query);
+            return $result;
+        }
         public function update_info_customer($data,$id){
             $customerName = mysqli_real_escape_string($this->db->link, $data['customerName']);
             $customerEmail = mysqli_real_escape_string($this->db->link, $data['customerEmail']);
             $customerPhone = mysqli_real_escape_string($this->db->link, $data['customerPhone']);            
             if(empty($customerName) || empty($customerEmail) || empty($customerPhone)){
-                $alert= "<span style='color:red;font-size:23px;'>Xin điền đầy đủ thông tin</span>";
+                $alert= "<span style='color:red;font-size:16px;'> Must be not empty</span>";
                 return $alert;
             }
             else{                 
                     $query ="UPDATE tbl_customer  SET customerName ='$customerName',customerEmail='$customerEmail',customerPhone='$customerPhone' WHERE customerID = '$id' ";
                     $result = $this->db->update($query);
                     if($result){
-                         $alert="<span style='color:green;font-size:23px;margin:2% 35%;'>Cập nhật thành công</span>";
+                         $alert="<span style='color:green;font-size:16px;margin:2% 20%;'> Update completion</span>";
                          return $alert;
                     }
                     else{
-                         $alert="<span style='color:red;font-size:23px;margin:2% 35%;'>Cập nhật thất bại</span>";
+                         $alert="<span style='color:red;font-size:16px;margin:2% 35%;'> Update not completion</span>";
                          return $alert;
+                    }
+            }
+        }
+         public function update_password_customer($data,$id){
+            $passwordOld = mysqli_real_escape_string($this->db->link, md5($data['passwordOld']));
+            $passwordNew = mysqli_real_escape_string($this->db->link, md5($data['passwordNew']));           
+            if(empty($passwordOld) || empty($passwordNew)){
+                $alert= "<span style='color:red;font-size:16px;margin-left: 21%;'> Bạn chưa điền đầy đủ thông tin!</span>";
+                return $alert;
+            }
+            else{ 
+                    $query_check = "SELECT * FROM tbl_customer WHERE customerID = '$id' ";
+                    $result_check = $this->db->select($query_check);
+                    $result_check_final =$result_check->fetch_assoc();
+                    if($result_check_final['password'] == $passwordOld){
+                        $query ="UPDATE tbl_customer  SET password='$passwordNew' WHERE customerID = '$id' ";
+                        $result = $this->db->update($query);
+                        if($result){
+                              $alert= "<span style='color:red;font-size:16px;margin-left: 21%;'>Thay đổi mật khẩu thành công!</span>";
+                              return $alert;
+                        }
+                        else{
+                              $alert= "<span style='color:red;font-size:16px;margin-left: 21%;'> Thay đổi mật khẩu không thành công!</span>";
+                              return $alert;
+                        }
+
+                    }
+                    else{
+                        $alert= "<span style='color:red;font-size:16px;margin-left: 21%;'>Thay đổi mật khẩu không thành công!</span>";
+                        return $alert;
                     }
             }
         }
@@ -131,9 +155,5 @@
             Session::set('customer_order',true); 
             header('Location:success.php');
         }
-
-
- 
-
     }
  ?>
